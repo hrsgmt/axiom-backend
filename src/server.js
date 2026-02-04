76fc8861d90427bc863cb2150acef6a82cf930ac
Axiom Dev <axiom@example.com>
2026-02-04 14:42:20 +0530

    Add auth register route

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 import Fastify from "fastify";
-import authRoutes from "./routes/auth.js";
+import authRoutes from "./routes/auth/auth.routes.js";
 
 const app = Fastify({ logger: true });
 const PORT = process.env.PORT || 4000;
 
-app.get("/", async () => {
-  return "Axiom backend running 🚀";
-});
+app.register(authRoutes, { prefix: "/api/auth" });
 
 app.get("/health", async () => {
   return { status: "ok" };
 });
-
-// AUTH ROUTES
-app.register(authRoutes, { prefix: "/api/auth" });
 
 app.listen({ port: PORT, host: "0.0.0.0" }, () => {
   console.log("Server running on port " + PORT);
