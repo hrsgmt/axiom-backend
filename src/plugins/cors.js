3a85cfc50d6e0ef892b5d891f47123058ba87b08
hrsgmt <hrsgmt@gmail.com>
2026-02-05 21:20:42 +0530

    Add CORS plugin

---
+++ src/plugins/cors.js
@@ -0,0 +1,9 @@
+import cors from "@fastify/cors";
+
+export default async function corsPlugin(app) {
+  await app.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  });
+}
