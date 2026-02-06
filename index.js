92a693f9f8bf7296c6c8aa4822d70fafd9b22f03
hrsgmt <hrsgmt@gmail.com>
2026-02-06 14:11:38 +0530

    FORCE Render entrypoint with raw HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-import './src/server.js';
+import http from "http";
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("RENDER SERVER IS RUNNING\n");
+}).listen(process.env.PORT || 4000, "0.0.0.0");
